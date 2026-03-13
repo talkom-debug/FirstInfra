8467ac51a553df680cd297ccc3317c5f2e87bcbd
Tal Komemi <talkom@cloudzone.io>
2026-03-13 17:00:43 +0200

    remove legacy terraform files

--- legacy/root-terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  description = "ALB public DNS name"
-  value       = aws_lb.app.dns_name
-}
-
-output "ecr_repository_url" {
-  description = "ECR repository URL"
-  value       = data.aws_ecr_repository.app.repository_url
-}
